a6654d61e9f1df6bb1eeca29aade7e8035053694
sahong <sahong@lkbt.co.kr>
2023-10-08 19:59:00 +0900

    [추가] vue프로젝트 생성

--- src/main/java/com/example/todolist/service/TodoServiceImpl.java
+++ src/main/java/com/example/todolist/service/TodoServiceImpl.java
@@ -5,11 +5,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * service interface 오버라이딩해야함.
+ */
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class TodoServiceImpl {
 
+    
    
 
     
